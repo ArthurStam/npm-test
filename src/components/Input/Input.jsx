5174e3445eaa2a765fac5f98de01f8415c49badc
ArthurStam <bumbledos@gmail.com>
2020-08-25 17:32:38 +0300

    input component

---
+++ src/components/Input/Input.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+Input.propTypes = {
+    mode: PropTypes.string
+}
+
+function Input({ children, mode, ...restProps }) {
+    return (
+        <button {...restProps}>{children}</button>
+    )
+}
+
+export default Input;
